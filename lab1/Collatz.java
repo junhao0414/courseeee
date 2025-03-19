281ce1b78816533c96a3de5b7b018bb233512fa6
JunHao <qjh3022823907@gmail.com>
2025-03-19 23:23:31 +0800

    Update Collatz.java
    Completed Lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,25 +3,22 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Correct implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2; // Even number: divide by 2
         } else {
-            return n * 2;
+            return 3 * n + 1; // Odd number: multiply by 3 and add 1
         }
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5; // Starting number
         System.out.print(n + " ");
         while (n != 1) {
-            n = nextNumber(n);
-            System.out.print(n + " ");
+            n = nextNumber(n); // Get the next number in the sequence
+            System.out.print(n + " "); // Print the next number
         }
-        System.out.println();
+        System.out.println(); // Print a new line after the sequence ends
     }
 }
-
